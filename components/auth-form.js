e7806c68ab4f9be8639c52f852ef1e34af8925a0
Ulia <lyanusia@gmail.com>
2025-09-29 18:56:38 +0200

    Switching Auth Modes With Query Parameters (Search Params)

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,11 +1,12 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { useFormState } from 'react-dom'
+import Link from "next/link";
+import { useFormState } from "react-dom";
 
-import { signup } from '../actions/auth-actions';
+import { signup } from "../actions/auth-actions";
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
+  // 'login', 'signup'
   const [formState, formAction] = useFormState(signup, {});
 
   return (
@@ -22,20 +23,24 @@ export default function AuthForm() {
         <input type="password" name="password" id="password" />
       </p>
       {formState.errors && (
-        <ul id='form-errors'>{
-          Object.keys(formState.errors).map(error =>
-            <li key={error}>
-              {formState.errors[error]}
-            </li>)
-            }
-      </ul>)}
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <button type="submit">
-          Create Account
+          {mode === "login" ? "Login" : "Create Account"}
         </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
