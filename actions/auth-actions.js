e5ba6a7214dfcd033c5a831666315a5f6dfe6b11
Ulia <lyanusia@gmail.com>
2025-09-29 19:02:35 +0200

    Adding User Login (via a Server Action)

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,40 +1,68 @@
-'use server';
-import { hashUserPassword } from '@/lib/hash';
-import { createUser } from '../lib/user';
-import { redirect } from 'next/navigation';
-import { createAuthSession } from '@/lib/auth';
+"use server";
+import { hashUserPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "../lib/user";
+import { redirect } from "next/navigation";
+import { createAuthSession } from "@/lib/auth";
 
 export async function signup(prevState, formData) {
-    const email = formData.get('email');
-    const password = formData.get('password');
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    let errors = {};
+  let errors = {};
 
-    if (!email.includes('@')) {
-        errors.email = 'Please enter a valid email address.';
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors }; // this object will become a new formState
+  }
+
+  const hashPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashPassword);
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "It seems like an account for chosen email already exists",
+        },
+      };
     }
+    throw error;
+  }
+}
 
-    if (password.trim().length < 8) {
-        errors.password = 'Password must be at least 8 characters long.';
-    }
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    if (Object.keys(errors).length > 0) {
-        return { errors }; // this object will become a new formState
-    }
+  const existingUser = getUserByEmail(email);
 
-    const hashPassword = hashUserPassword(password);
-    try {
-        const id = createUser(email, hashPassword);
-        await createAuthSession(id);
-        redirect('/training');
-    } catch (error) {
-        if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
-            return {
-                errors: {
-                    email: 'It seems like an account for chosen email already exists'
-                }
-            }
-        }
-        throw error;
-    }
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
 }
